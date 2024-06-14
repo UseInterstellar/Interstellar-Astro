fe040037ccea9a120c92e490af77c7020397b397
incognitotgt <tgt@incognitotgt.me>
2024-06-14 10:01:16 -0400

    prefetch

--- astro.config.ts
+++ astro.config.ts
@@ -4,5 +4,9 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  prefetch: {
+    defaultStrategy: "viewport",
+    prefetchAll: true
+  }
 });
