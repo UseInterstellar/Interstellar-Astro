2aadbc470aec055bb5588a4aaed63aa3921b81bf
incognitotgt <tgt@incognitotgt.me>
2024-06-21 18:05:07 -0400

    get asset fetching

--- index.ts
+++ index.ts
@@ -10,35 +10,35 @@ import Fastify from "fastify";
 import wisp from "wisp-server-node";
 const bare = createBareServer("/o/");
 const app = Fastify({
-  serverFactory: (handler) =>
-    createServer()
-      .on("request", (req, res) =>
-        bare.shouldRoute(req) ? bare.routeRequest(req, res) : handler(req, res),
-      )
-      .on("upgrade", (req, socket: Socket, head) =>
-        bare.shouldRoute(req)
-          ? bare.routeUpgrade(req, socket, head)
-          : wisp.routeRequest(req, socket, head),
-      ),
+	serverFactory: (handler) =>
+		createServer()
+			.on("request", (req, res) =>
+				bare.shouldRoute(req) ? bare.routeRequest(req, res) : handler(req, res),
+			)
+			.on("upgrade", (req, socket: Socket, head) =>
+				bare.shouldRoute(req)
+					? bare.routeUpgrade(req, socket, head)
+					: wisp.routeRequest(req, socket, head),
+			),
 });
 if (!fs.existsSync("dist")) {
-  console.error("Interstellar's not built yet! Building now...");
-  await build({}).catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+	console.error("Interstellar's not built yet! Building now...");
+	await build({}).catch((err) => {
+		console.error(err);
+		process.exit(1);
+	});
 }
 // @ts-ignore
 const ssrHandler = (await import("./dist/server/entry.mjs")).handler;
 await app
-  .register(fastifyStatic, {
-    root: path.join(import.meta.dirname, "dist", "client"),
-  })
-  .register(fastifyMiddie);
+	.register(fastifyStatic, {
+		root: path.join(import.meta.dirname, "dist", "client"),
+	})
+	.register(fastifyMiddie);
 app.use(ssrHandler);
 app.listen(
-  { port: Number.parseInt(process.env.PORT as string) || 8080 },
-  () => {
-    console.log("Listening on http://localhost:8080");
-  },
+	{ port: Number.parseInt(process.env.PORT as string) || 8080 },
+	() => {
+		console.log("Listening on http://localhost:8080");
+	},
 );
---
+++ src/pages/e/[...slug].ts
@@ -0,0 +1,35 @@
+import path from "node:path";
+import type { APIRoute } from "astro";
+import mime from "mime"
+
+export const GET: APIRoute = async ({ params }) => {
+  try {
+    const baseUrls = {
+      "1": "https://raw.githubusercontent.com/v-5x/x/fixy",
+      "2": "https://raw.githubusercontent.com/ypxa/y/main",
+      "3": "https://raw.githubusercontent.com/ypxa/w/master",
+    };
+    if (!params.slug) {
+      return new Response(null, { status: 404 });
+    }
+    if (!Object.keys(baseUrls).includes(params.slug.charAt(0))) {
+      return new Response("Invalid slug", { status: 400 });
+    }
+    const url = baseUrls[params.slug.charAt(0) as keyof typeof baseUrls] + params.slug.slice(1);
+    const asset = await fetch(url);
+    if (!asset.ok) {
+      return new Response("Error fetching data", { status: 500 });
+    }
+    const ext = path.extname(url)
+    const no = [".unityweb"]
+    const contentType = no.includes(ext) ? "application/octet-stream" : mime.getType(ext)
+    return new Response(asset.body, {
+      headers: {
+        "content-type": contentType || "application/octet-stream",
+      }
+    })
+  } catch (e) {
+    console.log(e)
+    return new Response("Error fetching data", { status: 500 });
+  }
+};
