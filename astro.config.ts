b18731acce9393345480542402a724aad397e510
incognitotgt <tgt@incognitotgt.me>
2024-06-21 17:16:40 -0400

    fortnite rizz party

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,41 @@
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
+import { createBareServer } from "@tomphttp/bare-server-node";
 import { defineConfig } from "astro/config";
-
+import wisp from "wisp-server-node";
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	adapter: node({
-		mode: "middleware",
-	}),
-	integrations: [tailwind({ applyBaseStyles: false })],
-	prefetch: {
-		defaultStrategy: "viewport",
-		prefetchAll: true,
-	},
+  output: "server",
+  adapter: node({
+    mode: "middleware",
+  }),
+  integrations: [tailwind({ applyBaseStyles: false })],
+  prefetch: {
+    defaultStrategy: "viewport",
+    prefetchAll: true,
+  },
+  vite: {
+    plugins: [
+      {
+        name: "vite-wisp-server",
+        configureServer(server) {
+          const bare = createBareServer("/o/");
+          server.httpServer?.on("upgrade", (req, socket, head) => {
+            if (req.url?.startsWith("/wisp")) {
+              wisp.routeRequest(req, socket, head);
+            } else if (bare.shouldRoute(req)) {
+              bare.routeUpgrade(req, socket, head);
+            }
+          });
+          server.middlewares.use((req, res, next) => {
+            if (bare.shouldRoute(req)) {
+              bare.routeRequest(req, res);
+            } else {
+              next();
+            }
+          })
+        },
+      },
+    ],
+  },
 });
--- index.ts
+++ index.ts
@@ -4,30 +4,41 @@ import type { Socket } from "node:net";
 import path from "node:path";
 import fastifyMiddie from "@fastify/middie";
 import fastifyStatic from "@fastify/static";
+import { createBareServer } from "@tomphttp/bare-server-node";
+import { build } from "astro";
 import Fastify from "fastify";
 import wisp from "wisp-server-node";
+const bare = createBareServer("/o/");
+const app = Fastify({
+  serverFactory: (handler) =>
+    createServer()
+      .on("request", (req, res) =>
+        bare.shouldRoute(req) ? bare.routeRequest(req, res) : handler(req, res),
+      )
+      .on("upgrade", (req, socket: Socket, head) =>
+        bare.shouldRoute(req)
+          ? bare.routeUpgrade(req, socket, head)
+          : wisp.routeRequest(req, socket, head),
+      ),
+});
 if (!fs.existsSync("dist")) {
-	console.error("Please run `pnpm build` first");
-	process.exit(1);
+  console.error("Interstellar's not built yet! Building now...");
+  await build({}).catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
 }
 // @ts-ignore
 const ssrHandler = (await import("./dist/server/entry.mjs")).handler;
-const app = Fastify({
-	serverFactory: (handler) =>
-		createServer()
-			.on("request", handler)
-			.on(
-				"upgrade",
-				(req, socket: Socket, head) =>
-					req.url?.startsWith("/srv") && wisp.routeRequest(req, socket, head),
-			),
-});
 await app
-	.register(fastifyStatic, {
-		root: path.join(import.meta.dirname, "dist", "client"),
-	})
-	.register(fastifyMiddie);
+  .register(fastifyStatic, {
+    root: path.join(import.meta.dirname, "dist", "client"),
+  })
+  .register(fastifyMiddie);
 app.use(ssrHandler);
-app.listen({ port: 8080 }, () => {
-	console.log("Listening on http://localhost:8080");
-});
+app.listen(
+  { port: Number.parseInt(process.env.PORT as string) || 8080 },
+  () => {
+    console.log("Listening on http://localhost:8080");
+  },
+);
