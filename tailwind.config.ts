8ad7b3b51638a0eb9007a8329a31cec1a65144b8
xbubbo <commits@gointerstellar.app>
2024-08-26 05:14:57 -0400

    chore: update asset card + start settings page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,16 @@ module.exports = {
       colors: {
         background: "var(--background)",
         secondary: "var(--background-secondary)",
+        settings: "var(--background-settings)",
         interactive: {
           DEFAULT: "var(--interactive)",
           secondary: "var(--interactive-secondary)",
         },
         border: "var(--border)",
-        accent: "var(--accent)",
+        accent: {
+          DEFAULT: "var(--accent)",
+          secondary: "var(--accent-secondary)",
+        },
         text: {
           DEFAULT: "var(--text)",
           secondary: "var(--text-secondary)",
