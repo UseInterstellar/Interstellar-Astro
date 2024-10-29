d972deddf66e957d81eb236f3a39855d3494542c
Bubbo <85169821+xbubbo@users.noreply.github.com>
2024-10-29 02:04:08 +0000

    remove cloaking from file and move to global

--- src/lib/global.ts
+++ src/lib/global.ts
@@ -36,6 +36,51 @@ document.addEventListener("astro:page-load", () => {
       localStorage.removeItem("hamburger-open");
     }
   });
+
+  // Popup behavior
+  if (!localStorage.getItem("ab")) localStorage.setItem("ab", "true");
+
+  if (
+    localStorage.getItem("ab") === "true" &&
+    window !== window.top &&
+    !navigator.userAgent.includes("Firefox")
+  ) {
+    const popup = open("about:blank", "_blank");
+    if (!popup || popup.closed) {
+      alert(
+        "Please allow popups for this site. Doing so will allow us to open the site in an about:blank tab and prevent this site from showing up in your history. You can turn this off in the site settings."
+      );
+    } else {
+      const doc = popup.document;
+      doc.title = localStorage.getItem("name") || "My Drive - Google Drive";
+      const iframe = doc.createElement("iframe");
+      Object.assign(iframe.style, {
+        width: "100%",
+        height: "100%",
+        border: "none",
+        outline: "none",
+        top: "0",
+        bottom: "0",
+        left: "0",
+        right: "0",
+        position: "fixed",
+      });
+      const link = Object.assign(doc.createElement("link"), {
+        rel: "icon",
+        href:
+          localStorage.getItem("icon") ||
+          "https://ssl.gstatic.com/docs/doclist/images/drive_2022q3_32dp.png",
+      });
+      doc.head.appendChild(link);
+      doc.body.appendChild(iframe);
+      const pLink = localStorage.getItem(encodeURI("pLink")) || "https://drive.google.com";
+      location.replace(pLink);
+
+      const script = doc.createElement("script");
+      script.textContent = `window.onbeforeunload=function(e){const n="Leave Site?";return(e||window.event).returnValue=n,n};`;
+      doc.head.appendChild(script);
+    }
+  }
 });
 
 // Panic
