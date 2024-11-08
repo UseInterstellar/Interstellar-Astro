ef74f108a6ceeba2557db34e02c3b84ad368b3d0
Bubbo <85169821+xbubbo@users.noreply.github.com>
2024-11-08 01:18:41 +0000

    Update routes when randomizing

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ import { build } from "astro";
 import Fastify from "fastify";
 import inConfig from "./config";
 
-const renamedFiles: { [key: string]: string } = {};
+const RenamedFiles: { [key: string]: string } = {};
 
 function RandomizeNames() {
   const pagesDir = path.join(process.cwd(), "src", "pages");
@@ -21,20 +21,49 @@ function RandomizeNames() {
       const randomName = `${Math.random().toString(36).slice(2, 11)}.astro`;
       const oldPath = path.join(pagesDir, file);
       const newPath = path.join(pagesDir, randomName);
-      renamedFiles[randomName] = file;
+      RenamedFiles[`/${file.replace(".astro", "")}`] =
+        `/${randomName.replace(".astro", "")}`;
       fs.renameSync(oldPath, newPath);
     }
   }
+
+  const AstroFiles = FindAstroFiles(process.cwd());
+  for (const astroFile of AstroFiles) {
+    let fileContent = fs.readFileSync(astroFile, "utf-8");
+
+    for (const [oldName, newName] of Object.entries(RenamedFiles)) {
+      const regex = new RegExp(`"${oldName}"`, "g");
+      fileContent = fileContent.replace(regex, `"${newName}"`);
+    }
+
+    fs.writeFileSync(astroFile, fileContent, "utf-8");
+  }
+}
+function FindAstroFiles(dir: string): string[] {
+  let results: string[] = [];
+  const list = fs.readdirSync(dir);
+
+  for (const file of list) {
+    const resolvedFile = path.resolve(dir, file);
+    const stat = fs.statSync(resolvedFile);
+
+    if (stat?.isDirectory()) {
+      results = results.concat(FindAstroFiles(resolvedFile));
+    } else if (file.endsWith(".astro")) {
+      results.push(resolvedFile);
+    }
+  }
+  return results;
 }
 
-function RevertNames() {
+function Revert() {
   const pagesDir = path.join(process.cwd(), "src", "pages");
 
-  for (const [randomName, originalName] of Object.entries(renamedFiles)) {
-    const oldPath = path.join(pagesDir, randomName);
-    const newPath = path.join(pagesDir, originalName);
-
-    fs.renameSync(oldPath, newPath);
+  for (const [oldPath, newPath] of Object.entries(RenamedFiles)) {
+    fs.renameSync(
+      path.join(pagesDir, `${newPath.replace("/", "")}.astro`),
+      path.join(pagesDir, `${oldPath.replace("/", "")}.astro`),
+    );
   }
 }
 
@@ -56,7 +85,7 @@ if (!fs.existsSync("dist")) {
     console.error(err);
     process.exit(1);
   });
-  RevertNames();
+  Revert();
 }
 
 await app.register(import("@fastify/compress"), {
