e683dfd5413610c4a3deab7e0c50d2b270fd7a76
Bubbo <85169821+xbubbo@users.noreply.github.com>
2024-11-19 14:52:16 +0000

    Fix images by restarting server on first build and disable telemetry

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import fs from "node:fs";
 import { createServer } from "node:http";
 import type { Socket } from "node:net";
 import path from "node:path";
+import { execSync } from "node:child_process";
 import fastifyMiddie from "@fastify/middie";
 import fastifyStatic from "@fastify/static";
 // @ts-expect-error shut
@@ -80,59 +81,71 @@ function Revert() {
   }
 }
 
-const port = Number.parseInt(process.env.PORT as string) || inConfig.port || 8080;
-const app = Fastify({
-  serverFactory: (handler) =>
-    createServer(handler).on("upgrade", (req, socket: Socket, head) =>
-      req.url?.startsWith("/f")
-        ? wisp.routeRequest(req, socket, head)
-        : socket.destroy(),
-    ),
-});
+async function Start() {
+  const FirstRun = process.env.FIRST === "true";
 
-// This is necessary to randomize the page names while preventing them from being committed by contributors.
-if (!fs.existsSync("dist")) {
-  RandomizeNames();
-  console.log("Interstellar's not built yet! Building now...");
-  await build({}).catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
-  Revert();
-}
+  if (!fs.existsSync("dist")) {
+    RandomizeNames();
+    console.log("Interstellar's not built yet! Building now...");
+    await build({}).catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+    Revert();
 
-await app.register(import("@fastify/compress"), {
-  encodings: ["br", "gzip", "deflate"],
-});
-
-if (inConfig.auth?.challenge) {
-  await app.register(import("@fastify/basic-auth"), {
-    authenticate: true,
-    validate(username, password, _req, _reply, done) {
-      for (const [user, pass] of Object.entries(inConfig.auth?.users || {})) {
-        if (username === user && password === pass) {
-          return done();
-        }
-      }
-      return done(new Error("Invalid credentials"));
-    },
-  });
-  await app.after();
-  app.addHook("onRequest", app.basicAuth);
-}
-
-// @ts-ignore
-const { handler } = await import("./dist/server/entry.mjs");
-await app
-  .register(fastifyStatic, {
-    root: path.join(import.meta.dirname, "dist", "client"),
-  })
-  .register(fastifyMiddie);
-app.use(handler);
-app.listen({ port }, (err, addr) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
+    if (FirstRun) {
+      console.log("Restarting Server...");
+      execSync("pnpm disable &&  pnpm start", { stdio: "inherit" });
+      process.exit(0);
+    }
   }
-  console.log("Listening on %s", addr);
-});
+
+  const port = Number.parseInt(process.env.PORT as string) || inConfig.port || 8080;
+  const app = Fastify({
+    serverFactory: (handler) =>
+      createServer(handler).on("upgrade", (req, socket: Socket, head) =>
+        req.url?.startsWith("/f")
+          ? wisp.routeRequest(req, socket, head)
+          : socket.destroy(),
+      ),
+  });
+
+  await app.register(import("@fastify/compress"), {
+    encodings: ["br", "gzip", "deflate"],
+  });
+
+  if (inConfig.auth?.challenge) {
+    await app.register(import("@fastify/basic-auth"), {
+      authenticate: true,
+      validate(username, password, _req, _reply, done) {
+        for (const [user, pass] of Object.entries(inConfig.auth?.users || {})) {
+          if (username === user && password === pass) {
+            return done();
+          }
+        }
+        return done(new Error("Invalid credentials"));
+      },
+    });
+    await app.after();
+    app.addHook("onRequest", app.basicAuth);
+  }
+
+  // @ts-ignore
+  const { handler } = await import("./dist/server/entry.mjs");
+  await app
+    .register(fastifyStatic, {
+      root: path.join(import.meta.dirname, "dist", "client"),
+    })
+    .register(fastifyMiddie);
+  app.use(handler);
+  app.listen({ port }, (err, addr) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log("Listening on %s", addr);
+  });
+}
+
+process.env.FIRST = process.env.FIRST || "true";
+await Start();
