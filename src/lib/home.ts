06e423a9578d7f664daf22ca7067a1aa129270a8
Bubbo <85169821+xbubbo@users.noreply.github.com>
2024-11-26 18:58:45 +0000

    Move home scripts to a seperate file

---
+++ src/lib/home.ts
@@ -0,0 +1,24 @@
+let searchUrl = localStorage.getItem("engine") || "https://www.google.com/search?q=";
+let input = document.getElementById("search") as HTMLInputElement | null;
+
+function isUrl(val: string = ""): boolean {
+  return /^http(s?):\/\//.test(val) || (val.includes(".") && !val.includes(" "));
+}
+
+document.addEventListener("astro:page-load", () => {
+  if (input) {
+    input.focus();
+    input.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        let url = input?.value || "";
+        if (!isUrl(url)) {
+          url = searchUrl + url;
+        } else if (!(url.startsWith("https://") || url.startsWith("http://"))) {
+          url = `https://${url}`;
+        }
+        sessionStorage.setItem("goUrl", url);
+        location.replace("/tabs");
+      }
+    });
+  }
+});
